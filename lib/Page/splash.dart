6124241dce7e4e2f40025500a0fb14ae3b88800f
zencode <zencode.id@gmail.com>
2022-10-18 14:23:43 +0700

    sudah add

--- lib/Page/splash.dart
+++ lib/Page/splash.dart
@@ -26,48 +26,62 @@ class _SplashPageState extends State<SplashPage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: darkColor,
+      // backgroundColor: darkColor,
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // CustomPaint(
-            //   painter: BackgroundPainter(),
-            //   child: Container(
-            //     width: double.infinity,
-            //     height: double.infinity,
-            //   ),
-            // ),
-            Container(
-              height: 180,
-              width: 180,
-              margin: EdgeInsets.only(bottom: 50, top: 250),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('images/sn.png'),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [0.1, 0.6, 0.9],
+              colors: [
+                Color(0xff182848),
+                Color(0xff334C83),
+                Color(0xff4B6CB7),
+              ],
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // CustomPaint(
+              //   painter: BackgroundPainter(),
+              //   child: Container(
+              //     width: double.infinity,
+              //     height: double.infinity,
+              //   ),
+              // ),
+              Container(
+                height: 180,
+                width: 180,
+                margin: EdgeInsets.only(bottom: 50, top: 250),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/sn.png'),
+                  ),
                 ),
               ),
-            ),
-            Text(
-              'SUARA NABAWIY 107.7 FM',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: whiteColor,
+              Text(
+                'SUARA NABAWIY 107.7 FM',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: whiteColor,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Damaikan Hati ,  Sejukan Jiwa',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: whiteColor,
+              SizedBox(
+                height: 20,
               ),
-            ),
-          ],
+              Text(
+                'Damaikan Hati ,  Sejukan Jiwa',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: whiteColor,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
